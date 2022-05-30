f8b528593844faba088db86a5940ecb2eaebf2d8
Shuvro Heemel <saklineheemel51@gmail.com>
2022-05-31 01:04:21 +0600

    cv done

---
+++ MvcApplication1/MvcApplication1/Controllers/CV_informationController.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcApplication1.Controllers
+{
+    public class CV_informationController : Controller
+    {
+        //
+        // GET: /CV_information/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Page1()
+        {
+            return View();
+        }
+        public ActionResult Profile()
+        {
+            return View();
+        }
+        public ActionResult Education()
+        {
+            return View();
+        }
+        public ActionResult Experience()
+        {
+            return View();
+        }
+        public ActionResult Technical_Skills()
+        {
+            return View();
+        }
+        public ActionResult Language()
+        {
+            return View();
+        }
+        public ActionResult References()
+        {
+            return View();
+        }
+        public ActionResult Projects()
+        {
+            return View();
+        }
+        public ActionResult Goal()
+        {
+            return View();
+        }
+    }
+}
