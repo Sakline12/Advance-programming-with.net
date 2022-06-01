034af1007d541e8a175255d5eed962119e22c7b4
Shuvro Heemel <saklineheemel51@gmail.com>
2022-06-02 03:40:04 +0600

    Value passing done

---
+++ Tempdata/Tempdata/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Tempdata.Models;
+
+namespace Tempdata.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Information()
+        {
+            Person pr = new Person()
+            {
+                Name = "P.K.M.Sakline Bari Heemel",
+                Age = "22",
+                Semester = "10th"
+            };
+            return View(pr);
+        }
+        public ActionResult Index()
+        {
+            TempData["msg"] = "Registration Successfull";
+            return RedirectToAction("Information");
+        }
+    }
+}
---
+++ Tempdata/Tempdata/Models/Person.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Tempdata.Models
+{
+    public class Person
+    {
+        public String Name { get; set; }
+        public String Age { get; set; }
+        public String Semester { get; set; }
+    }
+}
