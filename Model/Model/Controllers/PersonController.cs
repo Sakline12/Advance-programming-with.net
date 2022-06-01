fb37bd36bc47e3991f462886b630c45ff6e2ea3f
Shuvro Heemel <saklineheemel51@gmail.com>
2022-06-01 23:38:57 +0600

    Value passing done

---
+++ Model/Model/Controllers/PersonController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Model.Models;
+/*using Person.Models;*/
+
+namespace Model.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Information()
+        {
+            Person a1 = new Person()
+            {
+              Name="P.K.M.Sakline Bari Heemel",
+              Age="22",
+              Semester="11th"
+
+            };
+            return View(a1);
+        }
+    }
+}
---
+++ ViewBag/ViewBag/Controllers/PersonController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ViewBag.Controllers
+{
+    public class PersonController : Controller
+    {
+        // GET: Person
+        public ActionResult Information()
+        {
+            ViewBag.Name = "P.K.M.Sakline Bari Heemel";
+            ViewBag.Age = "22";
+            ViewBag.Semester = "10th";
+            return View();
+        }
+    }
+}
